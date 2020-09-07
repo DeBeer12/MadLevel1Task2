bfad39936e465fe97154b59b561d09b2ec9b2fa4
Richard <richardvdv105@gmail.com>
2020-09-07 14:24:13 +0200

    View binding

--- app/src/main/java/com/example/level1task2/MainActivity.kt
+++ app/src/main/java/com/example/level1task2/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.level1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.example.level1task2.databinding.ActivityMainBinding
 
@@ -12,8 +10,9 @@ private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val btnSubmit = binding.btnSubmit
         btnSubmit.setOnClickListener {
             checkAnswers()
